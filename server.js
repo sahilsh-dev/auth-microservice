c3329d07d182f06c9b3ec059d3f94fbcdc96195c
Sahil Sharma <vlsharma713@gmail.com>
2025-02-14 18:39:19 +0530

    Update package.json

--- server.js
+++ server.js
@@ -11,5 +11,5 @@ app.use(express.json());
 // Routes
 app.use("/api", require("./routes/userRoutes"));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
